be04462a26be013da2752aba3887e70c211e7a13
paranjay <paranjay.bind@gmail.com>
2020-10-07 04:51:34 +0530

    Made negligible progress but the path is clear now.
    Progress is progress anyways.
    #selfmotivation 100
    XD XD

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
-import discord, json
+import discord, json, os
 from discord.ext import commands
 
-TOKEN = 'process.env.BOT_TOKEN'
+TOKEN = os.environ['BOT_TOKEN']
 client = discord.Client()
-bot = commands.Bot(command_prefix='!')
+message: discord.Message
+user = discord.User
+channel = discord.channel
+bot = commands.Bot(command_prefix='#',)
 
 
 @bot.event
@@ -11,9 +14,14 @@ async def on_ready():
     print(f'Logged in as: {bot.user.name}')
     print(f'With ID: {bot.user.id}')
 
-@bot.command()
-async def mushimushi(ctx):
-    await ctx.send('Konnichiwa!')
+@bot.command(name="mushi", alias="konnichiwa")
+async def mushimushi(msg):
+    async with msg.typing():
+        author = msg.author
+        await msg.send(author.mention + ' Konnichiwa!')
+        
+
+
 
 
 # @client.event
